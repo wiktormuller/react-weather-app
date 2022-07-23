07d3dedbadb978d1d7cfc4a410e219310bdff949
Wiktor Muller <biznes.wiktor@gmail.com>
2022-07-24 00:37:14 +0200

    feat(client-app): Create first widget and city search in weather app

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8a47f29e30msha28448acaccb88bp18f985jsn192f692d293d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
